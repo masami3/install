9fc4dab6ac946d786bf3bce6f70f085e65473639
Masami OGAWA <ogawam606@gmail.com>
2024-09-02 16:14:53 +0900

    newer bin

--- circuitpython/install.sh
+++ circuitpython/install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 # circuitpython
 # esp32c6
+# https://circuitpython.org/board/espressif_esp32c6_devkitm_1_n4/
+
+download()
+{
+  wget https://downloads.circuitpython.org/bin/espressif_esp32c6_devkitm_1_n4/en_US/adafruit-circuitpython-espressif_esp32c6_devkitm_1_n4-en_US-9.1.3.bin
+}
 
 flash()
 {
-  esptool.py --chip esp32c6 --port /dev/ttyACM0 --baud 115200 --before default_reset --after hard_reset --no-stub write_flash --flash_mode dio 0x0 adafruit-circuitpython-espressif_esp32c6_devkitm_1_n4-en_US-9.1.1.bin
+  esptool.py --chip esp32c6 --port /dev/ttyACM0 --baud 115200 --before default_reset --after hard_reset --no-stub write_flash --flash_mode dio 0x0 adafruit-circuitpython-espressif_esp32c6_devkitm_1_n4-en_US-9.1.3.bin
 }
 
 com()
