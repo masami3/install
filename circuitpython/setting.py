573d4711085a76c830d0f6bf0c6497c5748539a7
Masami OGAWA <ogawam606@gmail.com>
2024-09-02 16:04:13 +0900

    wifi settings

---
+++ circuitpython/setting.py
@@ -0,0 +1,6 @@
+f = open('settings.toml', 'w')
+f.write('CIRCUITPY_WIFI_SSID = "YOUR_SSID"\n')
+f.write('CIRCUITPY_WIFI_PASSWORD = "YOUR_PASSWORD"\n')
+f.write('CIRCUITPY_WEB_API_PASSWORD = "YOUR_API_PASSWORD"\n')
+f.close()
+
