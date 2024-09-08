457eb644a5a69df793c6497304bde4cfd1f769d1
Masami OGAWA <ogawam606@gmail.com>
2024-09-09 06:40:46 +0900

    micropython build

---
+++ micropython/install.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# Usage: source ./install.sh
+
+set -euo pipefail
+
+require()
+{
+    sudo apt-get install build-essential libffi-dev git pkg-config
+    sudo apt-get install gcc-arm-none-eabi libnewlib-arm-none-eabi
+    sudo apt install qemu-system-arm
+}
+
+clone()
+{
+    cd ${HOME}/work
+    mkdir -p micropython
+    micropython/
+    mkdir 01
+    cd 01
+    git clone https://github.com/masami3/micropython
+    cd micropython/
+    git remote -v
+    git checkout -b dev-branch
+}
+
+build_cross()
+{
+    cd ${HOME}/work/micropython/01/micropython/mpy-cross
+    make
+}
+
+build_unix()
+{
+    cd ${HOME}/work/micropython/01/micropython/ports/unix
+    make submodules
+    make
+    ./build-standard/micropython -c "import sys; print(sys.version)"
+
+}
+
+build_qemu()
+{
+    cd ${HOME}/work/micropython/01/micropython/ports/qemu
+    make
+    make repl
+}
