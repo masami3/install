c7971884bfd2b76f4df381ea83fbda00703ee5be
Masami OGAWA <ogawam606@gmail.com>
2024-12-10 06:19:47 +0900

    fixed typos

--- editors/vzeditor/build.sh
+++ editors/vzeditor/build.sh
@@ -11,7 +11,7 @@ prepare()
 
 build()
 {
-  [ -e VA.MA- ] || cp -p VZEditor/SRC/VZ.MAK VZEditor/SRC/VZ.MA-
+  [ -e VZEditor/SRC/VZ.MA- ] || cp -p VZEditor/SRC/VZ.MAK VZEditor/SRC/VZ.MA-
   rm -f VZEditor/SRC/VZ.MAK
   cat VZEditor/SRC/VZ.MA- | dos2unix | sed -e 's/obj\\$/obj \\/' | unix2dos > VZEditor/SRC/VZ.MAK
 
