6213595b46a734b454047ac121ef80cc4765e173
Masami OGAWA <ogawam606@gmail.com>
2024-12-10 06:22:34 +0900

    removed useless cat.

--- editors/vzeditor/build.sh
+++ editors/vzeditor/build.sh
@@ -13,7 +13,7 @@ build()
 {
   [ -e VZEditor/SRC/VZ.MA- ] || cp -p VZEditor/SRC/VZ.MAK VZEditor/SRC/VZ.MA-
   rm -f VZEditor/SRC/VZ.MAK
-  cat VZEditor/SRC/VZ.MA- | dos2unix | sed -e 's/obj\\$/obj \\/' | unix2dos > VZEditor/SRC/VZ.MAK
+  dos2unix < VZEditor/SRC/VZ.MA- | sed -e 's/obj\\$/obj \\/' | unix2dos > VZEditor/SRC/VZ.MAK
 
   touch VZEditor/SRC/DUMMY
   cat > SETUP.BAT <<EOF
